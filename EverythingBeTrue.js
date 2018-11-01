e5167e17a44d7652bcca3f78517e8609111e8e2c
Desmond Harris <desmondharris912@gmail.com>
2018-11-01 14:06:46 -0500

    Create EverythingBeTrue.js
    There were two ways that I found to solve this. This way was cleaner using Array.prototype.every()

---
+++ EverythingBeTrue.js
@@ -0,0 +1,11 @@
+function truthCheck(collection, pre) {
+  // Is everyone being true?
+  return collection.every((ele) => {
+    return ele[pre];
+  });
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"},
+{"user": "Po", "sex": "female"}], "sex");
+
+
